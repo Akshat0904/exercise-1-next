49c2c0c1111a5853a54832409e3ab7409f039eed
Akshat0904 <askansara17@gmail.com>
2024-04-07 17:58:09 +0530

    Done Header

--- src/shared/component/header/header.constants.ts
+++ src/shared/component/header/header.constants.ts
@@ -1,3 +1,15 @@
-export const NAVLINKS : string[] = ['Buy', 'Sold', 'Rent', 'Estimate', 'More']
+import { ISidebarLink } from "./header.interface";
 
-export const SIDEBARLINKS  = [{src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-users.3124bf28.svg", title: "Find Agents"},{src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-auction.598d83df.svg", title: "Auction Results"},{src:"https://view.com.au/viewstatic/lancer/_next/static/media/nav-marker-pin.e61b8ed8.svg", title: "Location Profiles"}, {src:"https://view.com.au/viewstatic/lancer/_next/static/media/nav-lightbulb.120e73fc.svg", title:"Advice" }, {src:"https://view.com.au/viewstatic/lancer/_next/static/media/nav-file.0f7439ce.svg", title:"News" }, {src:"https://view.com.au/viewstatic/lancer/_next/static/media/nav-coins-hand.27bf8155.svg", title:"Home Loans" }, {src:"https://view.com.au/viewstatic/lancer/_next/static/media/nav-switch-horizontal-02.7e889623.svg", title:"Conveyancing" }, {src:"https://view.com.au/viewstatic/lancer/_next/static/media/nav-info-circle.ba9f7a09.svg", title:"About Us" },{src:"https://view.com.au/viewstatic/lancer/_next/static/media/nav-info-circle.ba9f7a09.svg", title:"Sitemap" }, ]
+export const NAVLINKS : string[] = ['Buy', 'Sold', 'Rent', 'Estimate', 'More']
+  
+  export const SIDEBARLINKS: ISidebarLink[] = [
+    { src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-users.3124bf28.svg", title: "Find Agents" },
+    { src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-auction.598d83df.svg", title: "Auction Results" },
+    { src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-marker-pin.e61b8ed8.svg", title: "Location Profiles" },
+    { src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-lightbulb.120e73fc.svg", title: "Advice" },
+    { src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-file.0f7439ce.svg", title: "News" },
+    { src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-coins-hand.27bf8155.svg", title: "Home Loans" },
+    { src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-switch-horizontal-02.7e889623.svg", title: "Conveyancing" },
+    { src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-info-circle.ba9f7a09.svg", title: "About Us" },
+    { src: "https://view.com.au/viewstatic/lancer/_next/static/media/nav-info-circle.ba9f7a09.svg", title: "Sitemap" }
+  ];
