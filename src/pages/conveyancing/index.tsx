c1807573d8d78c29356e182de040d88acd8ebc65
Akshat0904 <askansara17@gmail.com>
2024-04-10 10:01:47 +0530

    Create conveyancing route

---
+++ src/pages/conveyancing/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/src/shared/component/layout/Layout";
+
+const ConveyancingPage = () => {
+  return (
+    <Layout>
+      <div>index</div>;
+    </Layout>
+  );
+};
+
+export default ConveyancingPage;
